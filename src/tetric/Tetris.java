21381d3dd24966da321aa89a56a65be048371753
St <_rammstanya@mail.ru>
2019-10-12 14:57:26 +0300

    init Tetris

---
+++ src/tetric/Tetris.java
@@ -0,0 +1,17 @@
+package tetric;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Tetris extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.show();
+    }
+}
