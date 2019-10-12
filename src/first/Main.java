aae545f7585251de835dbff1d751d65763e9d6ba
St <_rammstanya@mail.ru>
2019-10-12 14:25:17 +0300

    using text font, rotate; not resizable stage

--- src/first/Main.java
+++ src/first/Main.java
@@ -3,6 +3,8 @@ package first;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -14,17 +16,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Text text = new Text("Hi from new Main");
-        text.setLayoutX(80);
+//        Text text = new Text("Ыыы съешь ещё этих мягких французских булок да выпей чаю");
+        Text text = new Text("Hi");
+        text.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+        text.setRotate(12);
+        text.setLayoutX(130);
         text.setLayoutY(100);
 
         Group group = new Group(text);
 
-        Scene scene = new Scene(group);
+        Scene scene = new Scene(group, 300, 400);
         stage.setScene(scene);
         stage.setTitle("Tetris");
         stage.setWidth(300);
-        stage.setHeight(250);
+        stage.setHeight(400);
+        stage.setResizable(false);
         stage.show();
     }
 }
